b97d5831d25b95962b8f3ce4ac174bd84c5324fc
more-ka <1640627628@qq.com>
2019-08-03 00:55:48 +0800

    父子组件之间互相传递消息

---
+++ src/components/parent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <span>我是父组件</span>-----{{fromSonMsg}}
+    <hr>
+    <son :msg="msgToSon" @handle="getMsgToSon"></son>
+  </div>
+</template>
+<script>
+import son from './son';
+export default {
+  name: 'parent',
+  data: function(){
+    return {
+      msgToSon: '父组件传给子组件的message',
+      fromSonMsg: ''
+    }
+  },
+  methods:{
+    getMsgToSon(value){
+      this.fromSonMsg = value
+    }
+  },
+  components:{
+    son
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/son.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <span>我是子组件</span>---{{msg}}<br>
+    <button @click="msgToParent">点击发送信息给父组件</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'son',
+  data:function(){
+    return {
+      sendMsg: '这是子组件传递给父组件的消息'
+    }
+  },
+  props:{
+    msg:{
+      type: String,
+      default: ''
+    }
+  },
+  methods:{
+    msgToParent:function(){
+      this.$emit('handle',this.sendMsg)
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
